5b667c0079d05a23deeed83144c2c2e60691026c
Marco Antônio <ma.souza.junior@gmail.com>
2026-07-30 17:58:36 -0300

    enh: add sub commands

--- cmd/sec.go
+++ cmd/sec.go
@@ -6,15 +6,40 @@ import (
 	"github.com/spf13/cobra"
 )
 
+var listSecCmd = &cobra.Command{
+	Use:   "ls",
+	Short: "list all secrets (value reducted)",
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("list secrets called")
+	},
+}
+
+var setSecCmd = &cobra.Command{
+	Use:   "set <secret> <value>",
+	Short: "set secret",
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("set var called")
+	},
+}
+
+var rmSecCmd = &cobra.Command{
+	Use:   "rm <secret>",
+	Short: "remove secret",
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("rm var called")
+	},
+}
+
 // secCmd represents the sec command
 var secCmd = &cobra.Command{
 	Use:   "sec",
 	Short: "secrets command",
-	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("sec called")
-	},
 }
 
 func init() {
+	secCmd.AddCommand(listSecCmd)
+	secCmd.AddCommand(setSecCmd)
+	secCmd.AddCommand(rmSecCmd)
+
 	rootCmd.AddCommand(secCmd)
 }
--- cmd/var.go
+++ cmd/var.go
@@ -6,15 +6,49 @@ import (
 	"github.com/spf13/cobra"
 )
 
+var listVarCmd = &cobra.Command{
+	Use:   "ls",
+	Short: "list all var envs on this vault",
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("list var called")
+	},
+}
+
+var setVarCmd = &cobra.Command{
+	Use:   "set <var> <value>",
+	Short: "set an environment variable",
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("set var called")
+	},
+}
+
+var getVarCmd = &cobra.Command{
+	Use:   "get <var>",
+	Short: "get environment variable",
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("get var called")
+	},
+}
+
+var rmVarCmd = &cobra.Command{
+	Use:   "rm <var>",
+	Short: "remove environment variable",
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("rm var called")
+	},
+}
+
 // varCmd represents the var command
 var varCmd = &cobra.Command{
 	Use:   "var",
 	Short: "variables command",
-	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("var called")
-	},
 }
 
 func init() {
+	varCmd.AddCommand(setVarCmd)
+	varCmd.AddCommand(getVarCmd)
+	varCmd.AddCommand(listVarCmd)
+	varCmd.AddCommand(rmVarCmd)
+
 	rootCmd.AddCommand(varCmd)
 }
