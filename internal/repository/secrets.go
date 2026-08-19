f1f21bcbf6a341363bdc1b2d0007d5847588b24e
Marco Souza <marco.souza@mongodb.com>
2026-08-19 17:55:11 -0300

    fea: crud implementation for repositories

--- internal/repository/secrets.go
+++ internal/repository/secrets.go
@@ -19,13 +19,68 @@ type Secret struct {
 }
 
 func (r *SecretRepository) GetByName(ctx context.Context, name string) (*Secret, error) {
-	query := "SELECT name, value FROM variables WHERE name = $1"
+	query := "SELECT name, value FROM secrets WHERE name = $1;"
 	row := r.db.QueryRowContext(ctx, query, name)
 
-	var variable Secret
-	if err := row.Scan(&variable.Name, &variable.EncryptedValue); err != nil {
+	var secret Secret
+	if err := row.Scan(&secret.Name, &secret.EncryptedValue); err != nil {
 		return nil, err
 	}
 
-	return &variable, nil
+	return &secret, nil
+}
+
+func (r *SecretRepository) Insert(ctx context.Context, name, value string) (*Secret, error) {
+	// FIXME: encrypt secret
+	encryptedValue := value
+
+	query := "INSERT INTO secrets (name, value) VALUES ($1, $2) RETURNING *;"
+	row := r.db.QueryRowContext(ctx, query, name, encryptedValue)
+
+	var secret Secret
+	if err := row.Scan(&secret.Name, &secret.EncryptedValue); err != nil {
+		return nil, err
+	}
+
+	return &secret, nil
+}
+
+func (r *SecretRepository) ListAll(ctx context.Context) (*[]Secret, error) {
+	query := "SELECT name FROM secrets;"
+	rows, err := r.db.QueryContext(ctx, query)
+	if err != nil {
+		return nil, err
+	}
+
+	var secrets []Secret
+
+	for rows.Next() {
+		var v Secret
+		if err := rows.Scan(&v.Name); err != nil {
+			return nil, err
+		}
+
+		secrets = append(secrets, v)
+	}
+
+	return &secrets, nil
+}
+
+func (r *SecretRepository) DeleteByName(ctx context.Context, name string) error {
+	query := "DELETE FROM secrets WHERE name = $1;"
+	result, err := r.db.ExecContext(ctx, query, name)
+	if err != nil {
+		return err
+	}
+
+	rowsAffected, err := result.RowsAffected()
+	if err != nil {
+		return err
+	}
+
+	if rowsAffected == 0 {
+		return ErrNotFount
+	}
+
+	return nil
 }
--- internal/repository/variables.go
+++ internal/repository/variables.go
@@ -29,3 +29,60 @@ func (r *VariableRepository) GetByName(ctx context.Context, name string) (*Varia
 
 	return &variable, nil
 }
+
+func (r *VariableRepository) Insert(ctx context.Context, name, value string) (*Variable, error) {
+	query := "INSERT INTO variables (name, value) VALUES (?, ?) RETURNING *;"
+	row := r.db.QueryRowContext(ctx, query, name, value)
+
+	var variable Variable
+	if err := row.Scan(&variable.Name, &variable.Value); err != nil {
+		return nil, err
+	}
+
+	return &variable, nil
+}
+
+func (r *VariableRepository) ListAll(ctx context.Context) (*[]Variable, error) {
+	query := "SELECT name, value FROM variables;"
+	rows, err := r.db.QueryContext(ctx, query)
+	if err != nil {
+		return nil, err
+	}
+
+	var variables []Variable
+
+	for rows.Next() {
+		var v Variable
+		if err := rows.Scan(&v.Name, &v.Value); err != nil {
+			return nil, err
+		}
+
+		variables = append(variables, v)
+	}
+
+	// interaction errors
+	if err := rows.Err(); err != nil {
+		return nil, err
+	}
+
+	return &variables, nil
+}
+
+func (r *VariableRepository) DeleteByName(ctx context.Context, name string) error {
+	query := "DELETE FROM variables WHERE name = $1;"
+	result, err := r.db.ExecContext(ctx, query, name)
+	if err != nil {
+		return err
+	}
+
+	rowsAffected, err := result.RowsAffected()
+	if err != nil {
+		return err
+	}
+
+	if rowsAffected == 0 {
+		return ErrNotFount
+	}
+
+	return nil
+}
